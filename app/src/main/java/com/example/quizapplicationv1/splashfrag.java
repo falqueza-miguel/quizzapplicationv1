20dc7633901a5db0046af9ce810c84a3deadb7de
Falqueza Miguel <falquezamiguel@gmail.com>
2020-10-09 16:21:14 +0800

    //splashfrag to menufrag working, account creation handled by firebase //no animations yet //no ui

---
+++ app/src/main/java/com/example/quizapplicationv1/splashfrag.java
@@ -0,0 +1,83 @@
+package com.example.quizapplicationv1;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class splashfrag extends Fragment {
+
+    private TextView splashFeedbackText;
+
+    private FirebaseAuth firebaseAuth;
+    private static final String START_TAG = "START_LOG";
+
+    private NavController navController;
+
+    public splashfrag() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_splashfrag, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        navController = Navigation.findNavController(view);
+
+        splashFeedbackText = view.findViewById(R.id.splashfrag_feedback);
+
+        splashFeedbackText.setText("checking user account");
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        FirebaseUser currentUser = firebaseAuth.getCurrentUser();
+        if (currentUser == null){
+
+            splashFeedbackText.setText("creating account");
+
+            //create account
+            firebaseAuth.signInAnonymously().addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if(task.isSuccessful()){
+                        splashFeedbackText.setText("account created");
+                        navController.navigate(R.id.action_splashfrag_to_menufrag);
+                    } else {
+                        Log.d(START_TAG,  "Start Log: " + task.getException());
+                    }
+                }
+            });
+        }else{
+            //navigate to menufrag
+            splashFeedbackText.setText("authenthicating user");
+            navController.navigate(R.id.action_splashfrag_to_menufrag);
+        }
+    }
+}
